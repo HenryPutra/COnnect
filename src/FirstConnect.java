cff5e95f1f91baecf3d8e75c55ca03a2c0386351
Hendri <henryputra071@gmail.com>
2023-03-17 15:55:38 +0700

    URL

---
+++ src/FirstConnect.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.URL;
+
+public class FirstConnect {
+    public static void main(String[] args) throws IOException {
+        ConnectURI koneksisaya = new ConnectURI();
+        URL myAddress = koneksisaya.buildURL
+                ("https://harber.mimoapps.xyz/api/getaccess.php");
+        String response = koneksisaya.getResponseFromHttpUrl(myAddress);
+        System.out.println(response);
+    }
+}
